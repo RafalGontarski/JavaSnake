187b6d99993823f40b55545723d6079434baa418
RafalGontarski <gontarskirafal@gmail.com>
2023-07-04 17:55:42 +0200

    Initialize apple position value in constructor.

--- src/main/java/org/example/model/Apple.java
+++ src/main/java/org/example/model/Apple.java
@@ -14,6 +14,8 @@ public class Apple {
         this.width = width;
         this.height = height;
         this.size = size;
+        position = new Point(0, 0);
+        newPosition();
     }
 
     public Point getPosition() {
