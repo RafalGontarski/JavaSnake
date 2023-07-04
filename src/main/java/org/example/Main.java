d4ee554ffb8b10a56629d9df47e87e8e4805f4c5
RafalGontarski <gontarskirafal@gmail.com>
2023-07-04 15:27:01 +0200

    Game Frame class has been created.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,16 @@
 package org.example;
 
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame ex = new GameFrame();
+            }
+        });
     }
 }
