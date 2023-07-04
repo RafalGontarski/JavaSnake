aba70bcb2cf8162f8c6dab8895e69e9fc311ff7e
RafalGontarski <gontarskirafal@gmail.com>
2023-07-04 15:31:23 +0200

    Game Frame checkApple method.

--- src/main/java/org/example/GameFrame.java
+++ src/main/java/org/example/GameFrame.java
@@ -1,6 +1,29 @@
 package org.example;
 
+import org.example.logic.GameLogic;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GameFrame extends JFrame {
+    private GameLogic gameLogic;
+    private Timer timer;
+
+    public GameFrame()  {
+        initGame();
+    }
+
+    private void initGame() {
+        gameLogic = new GameLogic();
+        timer = new Timer(140, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (gameLogic.isInGame()) {
+                    gameLogic.checkApple();
+                }
+            }
+        });
+    }
 }
